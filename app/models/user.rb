0ed916e182bd7beecd1590c3df666bd6883e7d0e
Eric Saupe <ericsaupe@gmail.com>
2022-09-17 11:59:41 -0700

    Add user contact info and encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
     :recoverable, :rememberable, :validatable,
     :confirmable, :lockable, :timeoutable, :trackable,
     :omniauthable
+
+  encrypts :email, :first_name, :last_name, deterministic: true
+  encrypts :phone
 end
--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -4,5 +4,8 @@ FactoryBot.define do
   factory :user do
     email { Faker::Internet.email }
     password { "test1234" }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    phone { Faker::PhoneNumber.phone_number }
   end
 end
