36f6513a976cf9a17b959b7d7ef1388b67300c5a
Eric Saupe <ericsaupe@gmail.com>
2022-10-11 13:03:30 -0700

    Add note to ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,6 +9,7 @@ class Ability
     can(:manage, Family, organization_id: user.organization_id)
     can(:manage, Search, organization_id: user.organization_id)
     can(:manage, Result, family_id: Family.where(organization_id: user.organization_id).pluck(:id))
+    can(:create, Note)
     return unless user.admin?
 
     can(:manage, Organization, id: user.organization_id)
