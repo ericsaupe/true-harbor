e37c3de9c563727f51f8740558e0a3af71315327
Eric Saupe <ericsaupe@gmail.com>
2022-11-02 15:12:01 -0700

    Better error handling of import

--- app/controllers/admin/imports_controller.rb
+++ app/controllers/admin/imports_controller.rb
@@ -9,8 +9,13 @@ module Admin
     def new; end
 
     def create
-      organization = current_user.super_admin? ? Organization.find(params[:organization_id]) : organization
-      CsvImporter::Idaho.import(organization, params[:file])
+      current_organization = current_user.super_admin? ? Organization.find(params[:organization_id]) : organization
+      begin
+        CsvImporter::Idaho.import(current_organization, params[:file])
+      rescue CSV::MalformedCSVError => e
+        flash[:error] = e.message
+        return redirect_to(new_admin_import_path)
+      end
       flash[:success] = "Imported #{params[:file].original_filename}"
       redirect_to(new_admin_import_path)
     end
