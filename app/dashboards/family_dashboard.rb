1b616356ef4ef25bdc59ab2f6dde29cec18155a3
Eric Saupe <ericsaupe@gmail.com>
2022-11-10 15:10:17 -0800

    Add availiabity to admin dashboard and remove family_interest

--- app/dashboards/family_dashboard.rb
+++ app/dashboards/family_dashboard.rb
@@ -16,17 +16,13 @@ class FamilyDashboard < Administrate::BaseDashboard
     available_counselor_transportation: Field::Boolean,
     available_school_transportation: Field::Boolean,
     available_visit_transportation: Field::Boolean,
+    availability: Field::Text,
     cats: Field::Boolean,
     city: Field::String,
     dogs: Field::Boolean,
     email: Field::String,
     exclusions: Field::HasMany,
     experience_with_care: Field::Text,
-    family_interest: Field::Select.with_options(searchable: false, collection: ->(field) {
-                                                                                 field.resource.class.send(
-                                                                                  field.attribute.to_s.pluralize,
-                                                                                ).keys
-                                                                               }),
     icwa: Field::Boolean,
     last_contacted_at: Field::DateTime,
     latitude: Field::String.with_options(searchable: false),
@@ -46,7 +42,7 @@ class FamilyDashboard < Administrate::BaseDashboard
                                                                       ).keys
                                                                     }),
     recreational_activities: Field::Text,
-    region: Field::String,
+    region: Field::BelongsTo,
     religion: Field::Select.with_options(searchable: false, collection: ->(field) {
                                                                           field.resource.class.send(
                                                                             field.attribute.to_s.pluralize,
@@ -72,27 +68,27 @@ class FamilyDashboard < Administrate::BaseDashboard
   #
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
-  COLLECTION_ATTRIBUTES = [:id, :name, :status, :city].freeze
+  COLLECTION_ATTRIBUTES = [:id, :name, :status, :region].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
-  SHOW_PAGE_ATTRIBUTES = [:id, :address_1, :address_2, :available_counselor_transportation,
+  SHOW_PAGE_ATTRIBUTES = [:id, :address_1, :address_2, :available_counselor_transportation, :availability,
                           :available_school_transportation, :available_visit_transportation, :cats, :city, :dogs,
-                          :email, :exclusions, :experience_with_care, :family_interest, :icwa, :last_contacted_at,
+                          :email, :exclusions, :experience_with_care, :icwa, :last_contacted_at,
                           :latitude, :license_date, :longitude, :name, :notes, :on_break_end_date,
                           :on_break_start_date, :organization, :other_animals, :other_children_in_home, :phone, :race,
                           :recreational_activities, :region, :religion, :results, :searches, :skills, :spots_available,
-                          :state, :status, :zip, :created_at, :updated_at,].freeze
+                          :state, :status, :zip, :created_at, :updated_at].freeze
 
   # FORM_ATTRIBUTES
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = [:address_1, :address_2, :available_counselor_transportation, :available_school_transportation,
-                     :available_visit_transportation, :cats, :city, :dogs, :email, :exclusions, :experience_with_care,
-                     :family_interest, :icwa, :last_contacted_at, :latitude, :license_date, :longitude, :name, :notes,
-                     :on_break_end_date, :on_break_start_date, :other_animals, :other_children_in_home,
-                     :phone, :race, :recreational_activities, :region, :religion, :results, :searches, :skills,
-                     :spots_available, :state, :status, :zip,].freeze
+                     :availability, :available_visit_transportation, :cats, :city, :dogs, :email, :exclusions,
+                     :experience_with_care, :icwa, :last_contacted_at, :latitude, :license_date,
+                     :longitude, :name, :notes, :on_break_end_date, :on_break_start_date, :other_animals,
+                     :other_children_in_home, :phone, :race, :recreational_activities, :region, :religion, :results,
+                     :searches, :skills, :spots_available, :state, :status, :zip,].freeze
 
   # COLLECTION_FILTERS
   # a hash that defines filters that can be used while searching via the search
