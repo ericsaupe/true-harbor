5ff5a549bc96e7d1407dbbed23d4c491674ec8fd
Eric Saupe <ericsaupe@gmail.com>
2022-12-29 14:12:30 -0700

    Add tests for timeframe selector

--- spec/system/analytics_spec.rb
+++ spec/system/analytics_spec.rb
@@ -47,5 +47,101 @@ RSpec.describe("Admin") do
       expect(page).to(have_content("Average Completed Search Times"))
       expect(page).to(have_content("15 minutes"))
     end
+
+    it "renders the average yes stat" do
+      yes_search = create(:search, organization: organization, completed_at: Time.zone.now)
+      create(:result, search: yes_search, state: "selected")
+      no_search = create(:search, organization: organization, completed_at: Time.zone.now)
+      create(:result, search: no_search, state: "declined")
+      not_finished_search = create(:search, organization: organization)
+      create(:result, search: not_finished_search)
+
+      visit("/analytics")
+      expect(page).to(have_content("Average Yes's per Search"))
+      within("#average-yes-per-search") do |element|
+        expect(element).to(have_content("0.5"))
+      end
+    end
+
+    it "renders the average no stat" do
+      no_search = create(:search, organization: organization, completed_at: Time.zone.now)
+      create(:result, search: no_search, state: "declined")
+      no_search = create(:search, organization: organization, completed_at: Time.zone.now)
+      create(:result, search: no_search, state: "declined")
+      yes_search = create(:search, organization: organization, completed_at: Time.zone.now)
+      create(:result, search: yes_search, state: "selected")
+
+      visit("/analytics")
+      expect(page).to(have_content("Average No's per Search"))
+      within("#average-no-per-search") do |element|
+        expect(element).to(have_content("0.67"))
+      end
+    end
+
+    it "renders the average time stat" do
+      Timecop.freeze do
+        create(:search, organization: organization, created_at: 1.day.ago, completed_at: 1.day.ago + 10.minutes)
+        create(:search, organization: organization, created_at: 1.day.ago, completed_at: 1.day.ago + 20.minutes)
+      end
+
+      visit("/analytics")
+      expect(page).to(have_content("Average Complete Time"))
+      within("#average-time-per-search") do |element|
+        expect(element).to(have_content("15 minutes"))
+      end
+    end
+
+    describe "timeframe" do
+      before do
+        Timecop.freeze do
+          create(:search, organization: organization, created_at: 1.year.ago, completed_at: 1.year.ago + 30.minutes)
+          create(:search, organization: organization, created_at: 1.year.ago, completed_at: 1.year.ago + 30.minutes)
+          create(:search, organization: organization, created_at: 8.days.ago, completed_at: 8.days.ago + 20.minutes)
+          create(:search, organization: organization, created_at: 8.days.ago, completed_at: 8.days.ago + 20.minutes)
+          create(:search, organization: organization, created_at: 1.day.ago, completed_at: 1.day.ago + 10.minutes)
+          create(:search, organization: organization, created_at: 1.day.ago, completed_at: 1.day.ago + 10.minutes)
+        end
+      end
+
+      it "renders the average time stat for the last 7 days" do
+        visit("/analytics")
+        expect(page).to(have_content("Average Complete Time"))
+        within("#average-time-per-search") do |element|
+          expect(element).to(have_content("20 minutes"))
+        end
+        select("Last 7 Days", from: "timeframe")
+        within("#average-time-per-search") do |element|
+          expect(element).to(have_content("10 minutes"))
+        end
+      end
+
+      it "renders the average time stat for the year to date" do
+        visit("/analytics")
+        expect(page).to(have_content("Average Complete Time"))
+        within("#average-time-per-search") do |element|
+          expect(element).to(have_content("20 minutes"))
+        end
+        select("Year to Date", from: "timeframe")
+        within("#average-time-per-search") do |element|
+          expect(element).to(have_content("15 minutes"))
+        end
+      end
+
+      it "renders the average time stat for all time" do
+        visit("/analytics")
+        expect(page).to(have_content("Average Complete Time"))
+        within("#average-time-per-search") do |element|
+          expect(element).to(have_content("20 minutes"))
+        end
+        select("Last 7 Days", from: "timeframe")
+        within("#average-time-per-search") do |element|
+          expect(element).to(have_content("10 minutes"))
+        end
+        select("All Time", from: "timeframe")
+        within("#average-time-per-search") do |element|
+          expect(element).to(have_content("20 minutes"))
+        end
+      end
+    end
   end
 end
