ba8c9564e2a986ef00d7e8ada552db31f152700e
Eric Saupe <ericsaupe@gmail.com>
2023-01-06 21:46:01 -0800

    Update DOM elements without websockets Refs #39

--- app/javascript/controllers/result_controller.js
+++ app/javascript/controllers/result_controller.js
@@ -6,7 +6,7 @@ export default class extends Controller {
     state: String
   }
 
-  static targets = [ "contactButton", "contactTime" ]
+  static targets = [ "button", "contactButton", "contactTime", "row" ]
 
   updateState(state) {
     if (this.stateValue === state) {
@@ -30,19 +30,73 @@ export default class extends Controller {
     }).then(_response => { this.stateValue = state })
   }
 
+  toggleClassesOnButton(target, classes, newState) {
+    const notSelectedClasses = ["bg-white", "hover:bg-pink-50", "text-gray-700"]
+    const selectedClasses = ["bg-emerald-500", "hover:bg-emerald-700", "text-white"]
+    const declinedClasses = ["bg-red-500", "hover:bg-red-700", "text-white"]
+    const waitingClasses  = ["bg-yellow-500", "hover:bg-yellow-700", "text-white"]
+
+    // Ensure we are working with a button
+    if (target.tagName !== "button") {
+      target = target.closest("button")
+    }
+
+    // Update the clicked button
+    if(this.stateValue === newState) {
+      target.classList.remove(...classes)
+      target.classList.add(...notSelectedClasses)
+    } else {
+      target.classList.add(...classes)
+      target.classList.remove(...notSelectedClasses)
+    }
+
+    // Update all other buttons
+    this.buttonTargets.forEach(button => {
+      if (button.dataset.value !== newState) {
+        button.classList.remove(...selectedClasses, ...declinedClasses, ...waitingClasses)
+        button.classList.add(...notSelectedClasses)
+      }
+    })
+  }
+
+  toggleClassesOnRow(classes, newState) {
+    const allClasses = ["bg-emerald-50", "bg-red-50", "bg-yellow-50", "bg-white"]
+    this.rowTarget.classList.remove(...allClasses)
+    if(this.stateValue === newState) {
+      this.rowTarget.classList.add("bg-white")
+    } else {
+      this.rowTarget.classList.add(...classes)
+    }
+  }
+
   toggleSelected(event) {
     event.preventDefault()
-    this.updateState("selected")
+    const newState = "selected"
+    const selectedButtonClasses = ["bg-emerald-500", "hover:bg-emerald-700", "text-white"]
+    const selectedRowClasses = ["bg-emerald-50"]
+    this.toggleClassesOnButton(event.target, selectedButtonClasses, newState)
+    this.toggleClassesOnRow(selectedRowClasses, newState)
+    this.updateState(newState)
   }
 
   toggleDeclined(event) {
     event.preventDefault()
-    this.updateState("declined")
+    const newState = "declined"
+    const declinedButtonClasses = ["bg-red-500", "hover:bg-red-700", "text-white"]
+    const declinedRowClasses = ["bg-red-50"]
+    this.toggleClassesOnButton(event.target, declinedButtonClasses, newState)
+    this.toggleClassesOnRow(declinedRowClasses, newState)
+    this.updateState(newState)
   }
 
   toggleWaiting(event) {
     event.preventDefault()
-    this.updateState("waiting")
+    const newState = "waiting"
+    const waitingButtonClasses = ["bg-yellow-500", "hover:bg-yellow-700", "text-white"]
+    const waitingRowClasses = ["bg-yellow-50"]
+    this.toggleClassesOnButton(event.target, waitingButtonClasses, newState)
+    this.toggleClassesOnRow(waitingRowClasses, newState)
+    this.updateState(newState)
   }
 
   updateContacted() {
